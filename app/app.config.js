4714814af6c6663e459223b64ac1539bdb3c116b
Guillaume NICOLAS <guillaume.nicolas@etu.univ-nantes.fr>
2016-08-30 17:42:41 +0200

    Remove node modules from repo + replace angular2/core dependency by @angular/core + remove Q dependency

--- app/app.config.js
+++ app/app.config.js
@@ -1,5 +1,5 @@
 "use strict";
-var core_1 = require('angular2/core');
+var core_1 = require('@angular/core');
 exports.APP_CONFIG = new core_1.OpaqueToken('app.config');
 exports.CONFIG = {
     appSetting: "my application setting"
--- app/app.config.ts
+++ app/app.config.ts
@@ -1,4 +1,4 @@
-import {OpaqueToken} from 'angular2/core';
+import {OpaqueToken} from '@angular/core';
 
 export let APP_CONFIG = new OpaqueToken('app.config');
 
