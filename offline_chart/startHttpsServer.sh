6a8ecd4626524e0063aed502e4f734566db11e0e
GABORIEAU Romain <rgaborieau@sqli.com>
2016-10-24 09:42:13 +0200

    Allegement du nombre de fichiers http-server n'avait pas besoin de fonctionner en tant que noeud.

--- offline_chart/startHttpsServer.sh
+++ offline_chart/startHttpsServer.sh
@@ -1,4 +1,4 @@
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
-cd http-server/public
-sudo node ../bin/http-server -p443 -S -C  ../bin/cert.pem -K ../bin/key.pem
+cd public
+sudo http-server -p443 -S -C ../certificates/cert.pem -K ../certificates/key.pem
--- offline_chart/startServer.sh
+++ offline_chart/startServer.sh
@@ -1,5 +1,5 @@
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
-cd http-server/public
-sudo node ../bin/http-server -p80
+cd public
+sudo http-server -p80
 #443 -S -C  ../bin/cert.pem -K ../bin/key.pem
