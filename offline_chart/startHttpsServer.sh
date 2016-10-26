b8d3789ca4bd74041e79422a75bd97020d910846
GABORIEAU Romain <rgaborieau@sqli.com>
2016-10-26 13:34:35 +0200

    ajout d'un script de generation de certificat auto-signe

--- offline_chart/startHttpsServer.sh
+++ offline_chart/startHttpsServer.sh
@@ -1,4 +1,4 @@
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
 cd public
-node ../../node_modules/http-server/bin/http-server -p443 -S -C ../certificates/cert.pem -K ../certificates/key.pem
+node ../../node_modules/http-server/bin/http-server -p443 -S -C ../certificates/certificate.crt -K ../certificates/private.key
