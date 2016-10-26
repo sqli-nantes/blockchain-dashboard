1bb618aa4dcbce0014496309bee029d59c032b1a
GABORIEAU Romain <rgaborieau@sqli.com>
2016-10-26 09:48:27 +0200

    Generification du docker-build-install

--- docker-build-install.sh
+++ docker-build-install.sh
@@ -1,5 +1,5 @@
-echo "nameserver 10.33.44.43" >> /etc/resolv.conf
-echo "nameserver 10.234.1.79" >> /etc/resolv.conf
+# Uncomment this line and modify XX.XX.XX.XX to match your DNS IP (duplicate the line if multiple DNS IP)
+# echo "nameserver XX.XX.XX.XX" >> /etc/resolv.conf
 
 cat /etc/resolv.conf
 
